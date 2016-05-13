2da20db7abb6c3ff465efb8ccb6f4d3d6c43a8ee
razor <razorcd@yahoo.com>
2016-05-13 19:29:33 +0200

    creates user resource

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < Object
+  def self.all
+    [
+      {name: "Name 1"},
+      {name: "Name 2"},
+      {name: "Name 3"},
+      {name: "Name 4"},
+      {name: "Name 5"},
+    ]
+  end
+end
